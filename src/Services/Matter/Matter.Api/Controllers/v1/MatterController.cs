e182a4588c345a7fc6df2cbafbd236b7c66e2506
LuisJimenez98 <luisjimenez9a@gmail.com>
2022-09-17 14:13:18 -0500

    Implementación funcionalidad buscar materia por identificador

--- src/Services/Matter/Matter.Api/Controllers/v1/MatterController.cs
+++ src/Services/Matter/Matter.Api/Controllers/v1/MatterController.cs
@@ -2,6 +2,7 @@
 using Matter.Application.Features.Commands.DeleteCreateMatterCommand;
 using Matter.Application.Features.Commands.UpdateMatterCommand;
 using Matter.Application.Features.Queries.GetAll;
+using Matter.Application.Features.Queries.GetById;
 using Matter.Application.Parameters;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -44,5 +45,13 @@ public class MatterController : BaseApiController
     {
         return Ok(await Mediator.Send(new GetAllQuery() { PageSize = filter.PageSize, PageNumber = filter.PageNumber }));
     }
+
+    //GET api/controller/5
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        return Ok(await Mediator.Send(new GetByIdQuery() { MateriaId = id }));
+    }
+
 
 }
--- src/Services/Matter/Matter.Application/Features/Queries/GetById/GetByIdQuery.cs
+++ src/Services/Matter/Matter.Application/Features/Queries/GetById/GetByIdQuery.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Matter.Application.Features.Queries.GetById
+﻿using AutoMapper;
+using Matter.Application.DTOs;
+using Matter.Application.Interfaces;
+using Matter.Application.Wrappers;
+using Matter.Domain.Entity;
+using MediatR;
+namespace Matter.Application.Features.Queries.GetById;
+public class GetByIdQuery: IRequest<Response<MateriaDto>>
 {
-    internal class GetByIdQuery
+    public int MateriaId { get; set; }
+}
+
+public class GetByIdQueryHandler : IRequestHandler<GetByIdQuery, Response<MateriaDto>>
+{
+    private readonly IRepository<Materia> _repository;
+    private readonly IMapper _mapper;
+    public GetByIdQueryHandler(IRepository<Materia> repository, IMapper mapper)
     {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    public async Task<Response<MateriaDto>> Handle(GetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var matter = await _repository.GetByIdAsync(request.MateriaId);
+        if (matter == null)
+            throw new KeyNotFoundException($"Registro no encontrado con el id {request.MateriaId}");
+        else
+        {
+            var matterDto = _mapper.Map<MateriaDto>(matter);
+            return new Response<MateriaDto>(matterDto);
+        }
+
     }
 }
